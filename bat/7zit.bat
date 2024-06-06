e82074cec9e1ab02789f44ff43947dc9392c47bb
iaueos <43596013+iaueos@users.noreply.github.com>
2024-06-06 10:07:33 +0700

    bat zip

--- bat/7zit.bat
+++ bat/7zit.bat
@@ -1,5 +1,6 @@
 @echo off
-SET ZZCMD="c:\Program Files\7-zip\7z.exe"
+SET ZZCMD="c:\Program Files\7-zip\7zG.exe"
+if "%1"=="" goto noParam 
 if "%2"=="" goto singleParam 
 if "%3"=="" goto duoParam
 
@@ -14,5 +15,15 @@ goto ende
 :singleParam
 set ft=%~d1%~p1%~n1.txt
  %ZZCMD% a %1 @"%ft%"
+
+:noParam
+echo 7zit [archive] 
+echo make [archive].7z from file list  in [archive].txt 
+echo.
+echo 7zit [archive] [listfile]
+echo make [archive].7z from [listfile].txt
+echo. 
+echo 7zit [archive] [listfile]  
+echo make [archive].[type] from [listfile].txt 
 
 :ende 
--- bat/new.bat
+++ bat/new.bat
@@ -4,4 +4,4 @@ IF NOT ""=="%1"  (
  SET PARAMDAY=%1
 )
 
-@forfiles /s /D +"%PARAMDAY%" /M *.* /C "cmd /c echo @relpath" | awk "{ print substr($0,4, length($0)-4) }"
+@forfiles /s /D +"%PARAMDAY%" /M *.* /C "cmd /c echo @relpath" | "c:\Program Files\Git\usr\bin\awk.exe" "{ print substr($0,4, length($0)-4) }"
