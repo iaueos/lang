9fa0a9849eeb6906e9b002380ec03c8c8a9aab16
iaueos <43596013+iaueos@users.noreply.github.com>
2024-06-06 10:09:33 +0700

    javascript examples

---
+++ js/local_storage__&__stringFormat.js
@@ -0,0 +1,64 @@
+myStorage = window.localStorage;
+
+
+localStorage.setItem('myCat', 'Tom');
+var cat = localStorage.getItem('myCat');
+localStorage.removeItem('myCat');
+// Clear all items
+localStorage.clear();
+
+
+function format(fmt, ...args) {
+    if (!fmt.match(/^(?:(?:(?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{[0-9]+\}))+$/)) {
+        throw new Error('invalid format string.');
+    }
+    return fmt.replace(/((?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{([0-9]+)\})/g, (m, str, index) => {
+        if (str) {
+            return str.replace(/(?:{{)|(?:}})/g, m => m[0]);
+        } else {
+            if (index >= args.length) {
+                throw new Error('argument index is out of range in format');
+            }
+            return args[index];
+        }
+    });
+}
+
+function print(fmt, ...args) {
+    console.log(format(fmt, ...args));
+}
+
+print("Hello, {0}! The answer is {1}.", "World", 42);
+print("{0} {1}", "{1}", 42);
+print("{{0}} will be replaced with {0}", 42);
+print("{0}} woops, throw!")
+
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number] 
+        : match
+      ;
+    });
+  };
+}
+
+if (!String.format) {
+  String.format = function(fmt, ...args) {
+     if (!fmt.match(/^(?:(?:(?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{[0-9]+\}))+$/)) {
+        throw new Error('invalid format string.');
+    }
+    return fmt.replace(/((?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{([0-9]+)\})/g, (m, str, index) => {
+        if (str) {
+            return str.replace(/(?:{{)|(?:}})/g, m => m[0]);
+        } else {
+            if (index >= args.length) {
+                throw new Error('argument index is out of range in format');
+            }
+            return args[index];
+        }
+    });
+  };
+}
